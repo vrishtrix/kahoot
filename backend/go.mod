7bd23f7d659c8df7abc04b5a6389a0f98a1f3b1d
Vrishin Patel <vrishin_patel@outlook.com>
2024-11-10 16:40:55 +0530

    create basic api route and connection to mongodb

--- backend/go.mod
+++ backend/go.mod
@@ -2,10 +2,14 @@ module github.com/Vrishtrix/kahoot
 
 go 1.23.3
 
-require github.com/gofiber/fiber/v2 v2.52.5
+require (
+	github.com/gofiber/fiber/v2 v2.52.5
+	go.mongodb.org/mongo-driver/v2 v2.0.0-beta2
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -15,5 +19,12 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,59 @@
 package main
 
 import (
+	"context"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"log"
 
-	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
+var quizCollection *mongo.Collection
+
+func setupDb() {
+	var client, err = mongo.Connect(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	quizCollection = client.Database("quiz").Collection("quizzes")
+}
+
+func getQuizzes(c *fiber.Ctx) error {
+	var ctx = context.Background()
+
+	var cursor, err = quizCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		log.Fatal("Error getting cursor while fetching quizzes: ", err)
+	}
+
+	var quizzes []fiber.Map
+	err = cursor.All(ctx, &quizzes)
+
+	if err != nil {
+		log.Fatal("Error fetching quizzes: ", err)
+	}
+
+	return c.JSON(quizzes)
+}
+
 func main() {
-	app := fiber.New()
+	var app = fiber.New()
+
+	setupDb()
+	app.Use(cors.New()) // Remove in production!
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	// API Route Handlers
+	app.Get("/api/quizzes", getQuizzes)
 
 	log.Fatal(app.Listen(":3000"))
 }
