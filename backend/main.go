54778d25249d54f3639fabccc0270d8d74b6720d
Vrishin Patel <vrishin_patel@outlook.com>
2024-11-15 02:46:48 +0530

    add a websocket connection handler

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"go.mongodb.org/mongo-driver/v2/bson"
@@ -24,36 +25,63 @@ func setupDb() {
 }
 
 func getQuizzes(c *fiber.Ctx) error {
-	var ctx = context.Background()
+	var (
+		cursor *mongo.Cursor
+		err    error
+		ctx    = context.Background()
+	)
 
-	var cursor, err = quizCollection.Find(ctx, bson.M{})
-
-	if err != nil {
+	if cursor, err = quizCollection.Find(ctx, bson.M{}); err != nil {
 		log.Fatal("Error getting cursor while fetching quizzes: ", err)
 	}
 
 	var quizzes []fiber.Map
-	err = cursor.All(ctx, &quizzes)
 
-	if err != nil {
-		log.Fatal("Error fetching quizzes: ", err)
+	if err = cursor.All(ctx, &quizzes); err != nil {
+		log.Fatal("Error getting all quizzes: ", err)
 	}
 
 	return c.JSON(quizzes)
 }
 
 func main() {
+	setupDb()
+
 	var app = fiber.New()
 
-	setupDb()
+	// Middlewares
 	app.Use(cors.New()) // Remove in production!
 
+	// HTTP Request Handlers (For serving frontend)
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
 	// API Route Handlers
 	app.Get("/api/quizzes", getQuizzes)
+
+	// WebSocket Connection Handler
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		var (
+			mt  int
+			msg []byte
+			err error
+		)
+
+		for {
+			if mt, msg, err = c.ReadMessage(); err != nil {
+				log.Println("Error reading from websocket: ", err)
+				break
+			}
+
+			log.Println("Message from client: ", string(msg))
+
+			if err = c.WriteMessage(mt, msg); err != nil {
+				log.Println("Error writing to client: ", err)
+				break
+			}
+		}
+	}))
 
 	log.Fatal(app.Listen(":3000"))
 }
