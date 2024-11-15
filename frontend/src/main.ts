577bd1994784537688b78cb5ec42f16ae70347a2
Vrishin Patel <vrishin_patel@outlook.com>
2024-11-15 12:24:07 +0530

    remove boilerplate code and add tailwindcss

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,10 @@
-import { mount } from 'svelte'
+import {mount} from 'svelte';
 import './app.css'
-import App from './App.svelte'
+import App from './App.svelte';
 
 const app = mount(App, {
-  target: document.getElementById('app')!,
-})
+	// biome-ignore lint/style/noNonNullAssertion: Boilerplate code
+	target: document.getElementById('app')!,
+});
 
-export default app
+export default app;
